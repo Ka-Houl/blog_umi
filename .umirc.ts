972b13c3c7d541f4cc5146d707411f27b50210c0
Ka-Houl <646676256@qq.com>
2020-07-15 01:47:23 +0800

    new commit

--- .umirc.ts
+++ .umirc.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   title: '个人博客',
 
   routes: pageRoutes,
-  // outputPath: 'blog', //指定输出路径。  //FIXME:设置这个属性，会使webpack热更新失效
+  outputPath: '/blog', //指定输出路径。  //FIXME:设置这个属性，会使webpack热更新失效
   chainWebpack(config, { webpack }) {
     config.optimization.splitChunks({
       chunks: 'async',
